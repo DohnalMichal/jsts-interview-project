b81c28449d57d813f3923c54e8bd0bc41f310ffe
DohnalMichal <michal.dohnal7@gmail.com>
2021-03-27 15:02:46 +0100

    ✨ Search user and user repos works

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,53 +1,87 @@
 import React, { useState, useEffect, useContext } from "react";
-import { TextField } from '@material-ui/core';
-
+import {
+  TextField,
+  Button,
+  Typography,
+  Card,
+  CardHeader,
+  Box
+} from "@material-ui/core";
+import UserCard from "./UserCard";
 import { getRepos, getUserData } from "../api/github-api";
-import { UserContext } from "./UserContext";
 
 const Searchbar = () => {
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
-  const [organisations, setOrganisations] = useState([]);
 
-  // const { username, setUsername, dataState } = useContext(UserContext);
-  const [userInput, setUserInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchUser();
+    searchRepos();
+  };
 
-  // const setData = ({ name }) => {
-  //   console.log(name);
-  //   setName(name);
-  // }
-
-  const handleSearch = e => {
-    setUserInput(e.target.value);
+  const searchUser = () => {
+    getUserData(username).then((data) => {
+      console.log(data.user);
+      setUser(data.user);
+    })
   }
-  const handleSubmit = e => {
-    getUserData(userInput)
-      .then(data => {
-        setUser(data);
-        console.log(data);
-      });
-    getRepos(userInput)
-      .then(data => {
-        setRepos(data);
-        console.log(data);
-      });
-  }
+  const searchRepos = () => {
+    setLoading(true);
+    getRepos(username).then((data) => {
+      setLoading(false);
+      setRepos(data);
+      console.log("Data:", data);
+    });
+    console.log("Repos: ", repos);
+  };
+
+  const renderRepo = (repo) => {
+    return (
+      <Card style={{ marginTop: "1em", padding: "1em" }}>
+        <Box display="flex" flexDirection="row">
+          {repo.description ? (
+            <Typography>{`${repo.name} ${repo.description}`}</Typography>
+          ) : (
+            <Typography>{`${repo.name} `}</Typography>
+          )}
+        </Box>
+      </Card>
+    );
+  };
+
   return (
-    <form
-      style={{ marginTop: 80 }}
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-      }}>
-      <TextField
-        color="primary"
-        id="outlined-basic"
-        label="Search Github User"
-        variant="outlined"
-        style={{ width: 400 }}
-        onChange={handleSearch}
-      />
-    </form>
+    <div>
+      <form
+        style={{ marginTop: 80, display: "flex" }}
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}>
+        <TextField
+          color="primary"
+          value={username}
+          id="outlined-basic"
+          label="Search Github User"
+          variant="outlined"
+          style={{ width: 400 }}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          style={{ marginLeft: "1em" }}>
+          {loading ? "Searching..." : "Search"}
+        </Button>
+      </form>
+      <UserCard user={user} />
+      {console.log(user)}
+      <Box display="flex" flexDirection="column">
+        {repos.map(renderRepo)}
+      </Box>
+    </div>
   );
 };
 
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import { Card, CardHeader, CardMedia, Box, Avatar } from '@material-ui/core';
-import { Component as RepoIcon } from '../icons/repository.svg'
-import { repos } from "./Searchbar";
+import React from "react";
+import { Card, CardHeader, CardMedia, Box, Avatar } from "@material-ui/core";
+// import { Component as RepoIcon } from '../icons/repository.svg'
+import { user } from "./Searchbar";
+// console.log(name);
 
-console.log(name);
+const UserCard = ({ user }) => {
+  console.log(user);
+  if (user.login) {
+    return (
+      <Card
+        style={{ marginTop: '2em' }}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center">
+          <Avatar
+            alt="avatar"
+            src={user.avatar_url}
+            style={{ height: '80px', width: '80px', marginTop: '1em' }} />
+          <CardHeader
+            title={user.login}
+            style={{ textAlign: 'center' }}
+          />
+        </Box>
+      </Card>
+    );
+  }
+  else {
+    return (<div></div>)
+  }
 
-const UserCard = () => {
-  return (
-    <Card
-      style={{ marginTop: '2em' }}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center">
-        <Avatar
-          alt="avatar"
-          src="https://avatars.githubusercontent.com/u/26080144?v=4"
-          style={{ height: '80px', width: '80px', marginTop: '1em' }} />
-        <CardHeader
-          title={name}
-          style={{ textAlign: 'center' }}
-        />
+};
 
-      </Box>
-    </Card>
-  )
-}
-
-export default UserCard
+export default UserCard;
